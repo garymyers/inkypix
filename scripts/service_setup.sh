1bf464077a84805fde5cae6963f01c4d75cfba9f
garymyers <42353927+garymyers@users.noreply.github.com>
2025-01-19 20:20:23 -0500

    Create service_setup.sh

---
+++ scripts/service_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo cp ./scripts/inkyweb.service /etc/systemd/system/
+sudo cp ./scripts/inkypix.service /etc/systemd/system/
+sudo systemctl daemon-reload
+
+sudo systemctl enable inkypix.service
+sudo systemctl enable inkyweb.service
