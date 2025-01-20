45234af24a6672f574c7f53a005dddb84a302bf9
Gary Myers <gmyers@gmail.com>
2025-01-19 21:11:46 -0500

    removed SUDO from script; just run as

--- scripts/service_setup.sh
+++ scripts/service_setup.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo cp ./scripts/inkyweb.service /etc/systemd/system/
-sudo cp ./scripts/inkypix.service /etc/systemd/system/
-sudo systemctl daemon-reload
+# run as SUDO
+cp ./scripts/inkyweb.service /etc/systemd/system/
+cp ./scripts/inkypix.service /etc/systemd/system/
+systemctl daemon-reload
 
-sudo systemctl enable inkypix.service
-sudo systemctl enable inkyweb.service
+systemctl enable inkypix.service
+systemctl enable inkyweb.service
