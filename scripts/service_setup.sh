30b46cb7bb2cf5a6ac88bfaf27114d62015f0818
Gary Myers <gmyers@gmail.com>
2025-01-19 21:13:51 -0500

    reverted

--- scripts/service_setup.sh
+++ scripts/service_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # run as SUDO
-cp ./scripts/inkyweb.service /etc/systemd/system/
-cp ./scripts/inkypix.service /etc/systemd/system/
-systemctl daemon-reload
+sudo cp ./scripts/inkyweb.service /etc/systemd/system/
+sudo cp ./scripts/inkypix.service /etc/systemd/system/
+sudo systemctl daemon-reload
 
-systemctl enable inkypix.service
-systemctl enable inkyweb.service
+sudo systemctl enable inkypix.service
+sudo systemctl enable inkyweb.service
